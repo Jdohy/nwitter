40323bb2478534509a8b2f2aa2ca9869b9770c48
Jdohy <doldol92877@gmail.com>
2023-06-28 13:47:30 +0900

    nwitter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
+import firebase from "./fbase";
+import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
